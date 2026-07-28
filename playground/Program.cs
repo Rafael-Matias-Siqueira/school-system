664204e810629032dfcdc4d58f456f25b53e3589
Rafael Matias <matiassiqueira07@gmail.com>
2026-07-28 20:32:24 -0300

    feat: Lugar de teste criado

---
+++ playground/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SchoolSystem.Tests;
+
+public class Sandbox
+{
+    public static void Main()
+    {
+        Console.WriteLine(char.IsLetter('a'));
+        Console.WriteLine(char.IsLetter('ç'));
+        Console.WriteLine(char.IsLetter('ã'));
+        Console.WriteLine(char.IsLetter('1'));
+        Console.WriteLine(char.IsLetter('@'));
+        Console.WriteLine(char.IsLetter('-'));
+    }
+}
